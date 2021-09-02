9a1fad3f09d7436fdc4ca5ba6cc9d11f6791b910
xxxkana <kana_m.0421@icloud.com>
2021-09-02 23:19:42 +0900

    軽微な修正

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -6,14 +6,17 @@ class RegistersController < ApplicationController
   end  
 
   def create
+    @user = User.find_by(id: params[:id])
     @register = Register.new(register_params)
     @register.save
     #redirect_to root_path
     #bypass_sign_in(current_user) 
   end
 
+  private
+
   def register_params
-    params.require(:register).permit(:user_id)
+    params.require(:register).permit(:user_id, :public_uid)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'registers#new'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update] 
   resources :registers, only: [:new, :create] 
 end
